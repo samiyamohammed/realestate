72d22c8571050a788c4846f681c7a14c226f5682
samiyamohammed <121340936+samiyamohammed@users.noreply.github.com>
2024-08-23 09:06:23 +0300

    Add files via upload

---
+++ lib/widgets/property_card.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import '../models/properties_list_model.dart';
+
+class PropertyCard extends StatelessWidget {
+  final Property property;
+  final bool showStatusTag; // New parameter to control the visibility of the status tag
+
+  const PropertyCard({
+    super.key,
+    required this.property,
+    this.showStatusTag = true, // Default value is true
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular(15.0),
+                ),
+                child: Image.asset(
+                  property.image,
+                  width: double.infinity,
+                  height: 70,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              if (showStatusTag) // Conditionally show the status tag
+                Positioned(
+                  top: 8,
+                  right: 8,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Text(
+                      property.status,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(1.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        property.name,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.favorite_border_outlined, color: Colors.red, size: 20),
+                      onPressed: () {
+                        // Implement your favorite functionality here
+                      },
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Icon(Icons.location_on, size: 12, color: Color.fromARGB(255, 0, 0, 0)),
+                    const SizedBox(width: 4),
+                    Expanded(
+                      child: Text(
+                        property.locationName,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Icon(Icons.monetization_on, size: 12, color: Color.fromARGB(255, 0, 0, 0)),
+                    const SizedBox(width: 4),
+                    Text(
+                      '${property.price.toStringAsFixed(2)} ETB',
+                      style: const TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
