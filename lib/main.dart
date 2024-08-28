5f52a22ae3f754c4c7aafa67faae6134244e0137
samiyamohammed <121340936+samiyamohammed@users.noreply.github.com>
2024-08-28 15:59:59 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:real_estate_marketplace/bloc/favorite_bloc/favorite_bloc.dart';
-import 'package:real_estate_marketplace/bloc/theme_bloc/theme_bloc.dart';
-import 'package:real_estate_marketplace/pages/account_page/account_page.dart';
-// ignore: unused_import
-import 'package:real_estate_marketplace/pages/favorites_page.dart';
-import 'package:real_estate_marketplace/pages/home_page.dart';
-// ignore: unused_import
-import 'package:real_estate_marketplace/pages/notification_page.dart';
+import 'package:real_estate_marketplace/bloc/home_bloc.dart';
+import 'package:real_estate_marketplace/config/router.dart'; // Import the router file here
 
 void main() {
-  runApp(MyApp());
+  runApp(const RealEstateApp());
 }
 
-class MyApp extends StatelessWidget {
+class RealEstateApp extends StatelessWidget {
+  const RealEstateApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => FavoritesBloc()),
-        BlocProvider(create: (context) => ThemeBloc()),
-      ],
-      child: BlocBuilder<ThemeBloc, ThemeState>(
-        builder: (context, themeState) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: themeState.themeData,
-            home: const AccountPage(),
-          );
-        },
+    return BlocProvider(
+      create: (context) => HomeBloc(),
+      child: MaterialApp.router(
+        title: 'Real Estate Marketplace',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routerConfig: router, // Use the router from the imported router file
       ),
     );
   }
