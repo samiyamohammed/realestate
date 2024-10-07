237f43bbb764a395963141b6827fdc45abe0d641
samiyamohammed <121340936+samiyamohammed@users.noreply.github.com>
2024-10-07 21:20:53 +0300

    Update faq_page.dart

--- lib/pages/faq_page.dart
+++ lib/pages/faq_page.dart
@@ -9,7 +9,7 @@ class FaqPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Name of the Real Estate App'),
+        title: const Text('FAQ'),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
@@ -19,65 +19,32 @@ class FaqPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "We are here to help you with anything and everything in 'App Name', "
-              "whether it is for buying, renting, or selling property of your choice. "
-              "Your Questions, Our Answers.",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black54,
+        child: ListView.builder(
+          itemCount: FaqProvider.faqItems.length,
+          itemBuilder: (context, index) {
+            final faq = FaqProvider.faqItems[index];
+            return Container(
+              margin: const EdgeInsets.symmetric(vertical: 5.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(color: Colors.grey[300]!),
               ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: double.infinity,
-              height: 50.0,
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search help',
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                    borderSide: const BorderSide(color: Colors.grey),
+              child: ExpansionTile(
+                title: Text(faq.question),
+                leading: Icon(faq.icon, size: 22, color: Colors.black54),
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      faq.answer, // Display the answer here
+                      style:
+                          const TextStyle(fontSize: 14, color: Colors.black87),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'FAQ',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 5),
-            Expanded(
-              child: ListView(
-                children: FaqProvider.faqItems.map((faq) {
-                  return Container(
-                    margin: const EdgeInsets.symmetric(vertical: 5.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      border: Border.all(color: Colors.grey[300]!),
-                    ),
-                    child: ListTile(
-                      title: Text(faq.question),
-                      leading: Icon(faq.icon, size: 22, color: Colors.black54),
-                      trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-                      onTap: () {
-                        // Handle tap event for each FAQ item
-                      },
-                    ),
-                  );
-                }).toList(),
-              ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
